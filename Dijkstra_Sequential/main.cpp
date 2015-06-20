22d9eb2c8f65a8ed39335f4eeafb115c6a4d9c33
shashi <shashi@rn-gpu.(none)>
2015-06-20 19:38:49 +0530

    Update 1

--- Dijkstra_Sequential/main.cpp
+++ Dijkstra_Sequential/main.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <list>
 
-class Node_t {
+class Node_t;
+typedef std::list<Node_t *> NodeList_t;
 
+class Node_t {
 public:    
     int key;
-    std::list<Node_t *> child;
-    int rank;   // Number of Children
+    NodeList_t child;
 
     Node_t(int k) {
         key = k;
-        rank = 0;
+    }
+
+    int getRank() {
+        return child.size();
     }
 
     void insert(Node_t *newNode) {
-        //child.add(newNode);
+        // Min-Heap insert
+        //child.push_back(newNode);
     }
 };
-typedef std::list<Node_t *> NodeList_t;
+
 class FibHeap {
 
-    std::list<Node_t *>root;
+    NodeList_t root;
     Node_t *min;
 
 public:
     FibHeap() {
         min = NULL;
-        //for (int i = 0; i < 10; i++)
-        //    root[0] = NULL;
     }
 
     void insert(int k) {
@@ -37,26 +40,69 @@ public:
         if (!min || min->key > k)
             min = newNode;
     }
-    int delete_min() {
+
+    void updateMin() {
         
+        int minKey = 9999;
         Node_t *minNode = NULL;
-        int minKey = 0;
         for (NodeList_t::iterator it = root.begin(); it != root.end(); ++it ) {
-            if ((**it).key < minKey) {
-                minKey = (**it).key;
+            if((*it)->key < minKey) {
                 minNode = *it;
             }
         }
-        if (!minNode) {
+        if (!minNode)
+            min = NULL;
+        else
+            min = minNode;
+    }
+
+    Node_t * merge(Node_t *node1, Node_t *node2) {
+        if (node1->key < node2->key) {
+            node1->child.push_back(node2);
+            return node1;
+        }
+        else {
+            node2->child.push_back(node1);
+            return node2;
+        }
+    }
+
+    void meld() {
+
+        Node_t *rank[100] = {};
+        Node_t *current, *mergedNode;
+        int i = 0, currentRank;
+
+        for (NodeList_t::iterator it = root.begin(); it != root.end(); ++it ) {
+            current = *it;
+            currentRank = current->getRank();
+            if (rank[currentRank] == NULL) {
+                rank[currentRank] = current;
+            } else {
+                mergedNode = merge(current, rank[currentRank]);
+                if (mergedNode == current)
+                    root.remove(current);
+                else
+                    root.remove(rank[currentRank]);
+            }
+        }
+    }
+
+    int delete_min() {
+        
+        if (!min) {
             std::cout << "No members in the heap. No Min element found\n";
             return 1;
         }
         
-        for (NodeList_t::iterator it = minNode->child.begin(); it != minNode->child.end(); ++it ) {
+        for (NodeList_t::iterator it = min->child.begin(); it != min->child.end(); ++it ) {
             root.push_back(*it);
         }
-        root.remove(minNode);
-        return 1;
+        root.remove(min);
+        updateMin();
+
+        meld();
+        return 0;
     }
 
     void printFBHeap() {
@@ -70,11 +116,12 @@ public:
 int main() {
 
     FibHeap FH;
-    FH.insert(3);
-    FH.insert(4);
-    FH.insert(6);
     FH.insert(8);
     FH.delete_min();
+    FH.insert(4);
+    FH.insert(6);
+    FH.delete_min();
+    FH.insert(3);
     FH.insert(9);
     
     FH.printFBHeap();
